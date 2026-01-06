dbcce833cda54116e212f671cc8a066c1f9f34a4
Nikolay <kny981@gmail.com>
2026-01-06 21:23:24 +0300

    update cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -15,7 +15,7 @@ mongoose.connect(DB_ADDRESS);
 
 // Только для локальных тестов. Не используйте это в продакшене
 app.use(cors({
-    origin: 'https://klimov.project.nomorepartiessbs.ru', // фронт
+    origin: 'http://klimov.project.nomorepartiessbs.ru', // фронт
     methods: ['GET','POST','PUT','DELETE'],
     credentials: true
 }))
