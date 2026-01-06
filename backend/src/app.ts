6972de7ad49257473ef9abe62c8b5f5dc4d1c4e2
Nikolay <kny981@gmail.com>
2026-01-07 02:29:53 +0300

    update

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -19,6 +19,7 @@ app.use(cors({
     methods: ['GET','POST','PUT','DELETE'],
     credentials: true
 }))
+app.options('*', cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
