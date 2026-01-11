88553bc08ff2ef42660c483427644868de9fab88
Nickolay <company22105@gmail.com>
2026-01-11 12:09:45 +0000

    added pm2 configs

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master', DEPLOY_REPO,
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'api-service',
+  }],
+
+  // Настройка деплоя
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'post-deploy': 'cd ../nodejs-pm2-deploy && npm i && npm run build',
+    },
+  },
+};
